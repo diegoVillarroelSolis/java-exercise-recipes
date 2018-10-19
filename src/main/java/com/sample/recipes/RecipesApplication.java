b8a51382c67141c9be879b505deac68b3b8d3b88
Diego Villarroel <diego.villarroel@truextend.com>
2018-10-19 16:09:32 -0400

    JwtAuthenticationSupport: Add jwt filters

--- src/main/java/com/sample/recipes/RecipesApplication.java
+++ src/main/java/com/sample/recipes/RecipesApplication.java
@@ -2,9 +2,16 @@ package com.sample.recipes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class RecipesApplication {
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecipesApplication.class, args);
